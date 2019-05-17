91f599bcec48633ae1358508b1ff004aed2e51ce
petitagneaunoir <petitagneaunoir@gmail.com>
2019-05-17 18:53:41 -0400

    initialize repository

---
+++ index.js
@@ -0,0 +1,33 @@
+// Reverses a string.
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent === this.processedContent().reverse();
+  }
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+
+}
+
+TranslatedPhrase.prototype = new Phrase();
